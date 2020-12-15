796a9ebc6975e7436c0a110e36ab34a6c5403e85
oozsen <oozsen@gmail.com>
2020-12-15 12:07:17 +0300

    * CollectionViewPage.xaml: CollectionViewPage eklendi
    * ListViewPage.xaml.cs:
    * CollectionViewPage.xaml.cs:
    
    * ListViewPage.xaml: ListViewPage eklendi

---
+++ SwipeViewDemo2/CollectionViewPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SwipeViewDemo2
+{
+    public partial class CollectionViewPage : ContentPage
+    {
+        public CollectionViewPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
